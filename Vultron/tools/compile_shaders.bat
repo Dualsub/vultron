135b5049d4d5568a176d419956fb1abb49517376
Simon Sjöling <rizedualsub@gmail.com>
2024-05-18 13:57:42 +0200

    Add compute shaders to compilation

--- Vultron/tools/compile_shaders.bat
+++ Vultron/tools/compile_shaders.bat
@@ -1,5 +1,5 @@
 @echo off
-FOR %%G IN (../assets/shaders/*.vert, ../assets/shaders/*.frag) DO (
+FOR %%G IN (../assets/shaders/*.vert, ../assets/shaders/*.frag, ../assets/shaders/*.comp) DO (
     glslc.exe --target-env=vulkan ../assets/shaders/%%G -o ../assets/shaders/%%G.spv
     echo Compiled ../assets/shaders/%%G
 )

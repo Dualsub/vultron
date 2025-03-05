379af6aff60cc4b4ae9b98d2a59c99028284501b
Simon Sjöling <rizedualsub@gmail.com>
2025-03-05 12:58:49 +0100

    Update emissive color to default to white in Types.h

--- Vultron/include/Vultron/Types.h
+++ Vultron/include/Vultron/Types.h
@@ -43,7 +43,7 @@ namespace Vultron
         glm::vec2 texCoord = glm::vec2(0.0f);
         glm::vec2 texSize = glm::vec2(1.0f);
         glm::vec4 color = glm::vec4(1.0f);
-        glm::vec4 emissiveColor = glm::vec4(0.0f);
+        glm::vec4 emissiveColor = glm::vec4(1.0f);
         bool castShadows = true;
 
         // Compute hash of mesh and texture
@@ -78,7 +78,7 @@ namespace Vultron
         std::vector<AnimationInstance> animations = {};
         std::array<int32_t, 3> bonesToIgnore = {-1, -1, -1};
         glm::vec4 color = glm::vec4(1.0f);
-        glm::vec4 emissiveColor = glm::vec4(0.0f);
+        glm::vec4 emissiveColor = glm::vec4(1.0f);
 
         // Compute hash of mesh and texture
         uint64_t GetHash() const
